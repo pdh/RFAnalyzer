8731e1a5665e4c13cabb176d2187b3a48e69f933
pdh <1621357+pdh@users.noreply.github.com>
2025-02-20 21:20:39 -0800

    wip for cw

--- app/src/main/java/com/mantz_it/rfanalyzer/AnalyzerProcessingLoop.java
+++ app/src/main/java/com/mantz_it/rfanalyzer/AnalyzerProcessingLoop.java
@@ -15,20 +15,20 @@ import java.util.concurrent.TimeUnit;
  *              waterfall display a linear time scale.
  *
  * @author Dennis Mantz
- *
+ * <p>
  * Copyright (C) 2014 Dennis Mantz
- * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
- *
+ * License: <a href="http://www.gnu.org/licenses/gpl.html">...</a> GPL version 2 or higher
+ * <p>
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
